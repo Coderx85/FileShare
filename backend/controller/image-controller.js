aa56cdc95ac3d54701740f0e2c64c2679bf04d41
Priyanshu085 <abpriyanshu007@gmail.com>
2023-09-13 01:08:54 +0530

    Add LICENSE

--- backend/controller/image-controller.js
+++ backend/controller/image-controller.js
@@ -1,34 +1,44 @@
-import File from "../models/file.js";
+    import File from "../models/file.js";
 
-export const uploadImage = async (request, response) => {
-    // return response.status(200).json({ msg: 'Hello' })
-    const fileObj = {
-        path: request.file.path,
-        name: request.file.originalname
+    export const uploadImage = async (request, response) => {
+        // return response.status(200).json({ msg: 'Hello' })
+        const fileObj = {
+            path: request.file.path,
+            name: request.file.originalname
+        }
+
+        try {
+            const file = await File.create(fileObj);
+            console.log(file);  
+
+            // const filePath = `http://localhost:8500/file/${file._id}`; // Adjust the URL as needed
+            
+            // Extract the file_id from the response data path
+            // const file_id = response.data.path.split('/').pop();
+            
+            // Set the result state with the relative path
+            // setResult(`/file/${file._id}`);
+            
+            response.status(200).json({ path: `http:localhost:8500/file/${file._id}` })
+            // setResult(response);
+        } catch (error) {
+            console.error(error.message);
+            response.status(500).json({ error: error.message })
+        }
     }
 
-    try {
-        const file = await File.create(fileObj);
-        console.log(file);
-        response.status(200).json({ path: `https:localhost:8500/file/${file_id}` })
-    } catch (error) {
-        console.error(error.message);
-        response.status(500).json({ error: error.message })
+    export const downloadImage = async(request, response) => {
+        try {
+            const file = await File.findById(request.params.fileId);
+
+            file.downloadContent++;
+
+            await file.save();
+
+            response.download(file.path, file.name);
+
+        } catch (error) {
+            console.error(error.message);
+            return response.status(500).json({ error: error.message })
+        }
     }
-}
-
-export const downloadImage = async(request, response) => {
-    try {
-        const file = await File.findById(request.params.fileId);
-
-        file.downloadContent++;
-
-        await file.save();
-
-        response.download(file.path, file.name);
-
-    } catch (error) {
-        console.error(error.message);
-        return response.status(500).json({ error: error.message })
-    }
-}
